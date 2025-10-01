ba85f048834f54631ac4054573632ecfeb2f655a
Костюшин Валерий <v.kostyushin@domain.local>
2025-10-01 13:30:36 +0300

    add cases

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
+            while (true) { 
+                System.out.println("Выбор действия:");
+                int caseNumber = console.nextInt();  
+                switch (caseNumber) {
+                    case 0:
+                    break;
+                
+                    case 1:
+                    System.out.println("type");
+                    int caseNumber1 = console.nextInt();
+                    switch (caseNumber1) {
+                        case 1:
+                        //Book
+                            break;
+                        case 2:
+                        //Newspaper
+                        break;
+                        case 3:
+                        //Magazine 
+                        break;
+                    }
+                    break;
+
+                    case 2:
+                    break;
+
+                    case 3:
+                    break;
+
+                    case 4:
+                    System.out.println();
+                    break;
+                    
+                
+            }
     }
+}
 }
 
 
